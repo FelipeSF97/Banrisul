bd0aa9598345e854f31d2e33c7217ef2f88f7884
Felipe Flores <143096126+FelipeSF97@users.noreply.github.com>
2025-10-24 12:59:47 -0300

    Adicionando arquivos da primeira aula

---
+++ Aula_01/01_atribuicao_de_variaveis_e_constantes/01_atribuicao_de_variaveis_e_constantes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_atribuicao_de_variaveis_e_constantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int capacidadeMaxima = 5000;
+            int ingressosVendidos = 2000;
+            int ingressosRestantes = capacidadeMaxima - ingressosVendidos;
+
+            System.Console.WriteLine($"Ingressos Vendidos: {ingressosVendidos} de {capacidadeMaxima}");
+            System.Console.WriteLine($"Ingressos Restantes: {ingressosRestantes}");
+        }
+    }
+}
---
+++ Aula_01/02_tipagem_de_dados/02_tipagem_de_dados/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_tipagem_de_dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //char emoji = '😊';
+            //System.Console.WriteLine($"{emoji}");
+            //Too many characters in character literal
+
+            char emoji = '☺';
+            System.Console.WriteLine($"{emoji}");
+            //☺
+
+            char letra = 'A';
+            int numero = letra;
+            System.Console.WriteLine($"{numero}");
+            //65
+
+            string condicao = "TRUE";
+            bool converte = System.Convert.ToBoolean(condicao);
+            System.Console.WriteLine($"{converte}");
+            //True
+
+            double pontosGanhos = 84.68;
+            int barrasChocolate = System.Convert.ToInt32(pontosGanhos);
+            System.Console.WriteLine($"{barrasChocolate}");
+            //85
+        }
+    }
+}
